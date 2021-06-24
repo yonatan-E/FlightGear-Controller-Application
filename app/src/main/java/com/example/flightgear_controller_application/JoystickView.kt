e36cf57fd537fde65dbdb13a5fec281454ce60d4
ilandovprais <ilandovprais@gmail.com>
2021-06-24 16:14:03 +0300

    working on joystick

--- app/src/main/java/com/example/flightgear_controller_application/JoystickView.kt
+++ app/src/main/java/com/example/flightgear_controller_application/JoystickView.kt
@@ -1,33 +1,44 @@
 package com.example.flightgear_controller_application
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
 import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 
-class JoystickView : SurfaceView{
+class JoystickView : SurfaceView, SurfaceHolder.Callback{
 
-    constructor(ctx: Context) : super(ctx)
+    constructor(ctx: Context) : super(ctx){
+        holder.addCallback(this)
+    }
 
     constructor(ctx: Context, attrs: AttributeSet) : super(ctx, attrs){
-
+        holder.addCallback(this)
     }
 
-    constructor(ctx: Context, attrs: AttributeSet, style: Int) : super(ctx, attrs, style)
+    constructor(ctx: Context, attrs: AttributeSet, style: Int) : super(ctx, attrs, style){
+        holder.addCallback(this)
+    }
 
     private fun drawJoystick(newX: Float, newY: Float){
+        if(holder.surface.isValid()){
+            var canvas = this.holder.lockCanvas()
+            var colors = Paint()
+            canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
+        }
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder){
 
     }
 
-    fun surfaceCreated(holder: SurfaceHolder){
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int){
 
     }
 
-    fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int){
-
-    }
-
-    fun surfaceDestroyed(holder: SurfaceHolder){
+    override fun surfaceDestroyed(holder: SurfaceHolder){
 
     }
 
